4367682b4ac263787142f63baa5430d3596d4cdc
Likhitha <Likhitha@LAPTOP-UR81DCK7>
2023-09-27 14:30:40 +0530

    demo

---
+++ devops/src/devops/demo.java
@@ -0,0 +1,11 @@
+package devops;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is first repository");
+
+	}
+
+}
